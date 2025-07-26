ef4608ac74f791daa22d8902ab9131fb42f5c73b
root <root@vmi2695393.contaboserver.net>
2025-07-26 13:28:59 +0200

    Add server deployment configuration for Contabo VPS

--- Mekawy_ERP/settings.py
+++ Mekawy_ERP/settings.py
@@ -14,6 +14,7 @@ import os
 from pathlib import Path
 import environ
 import requests
+from decouple import config
 
 
 # Initialize environment variables
@@ -128,12 +129,14 @@ AUTHENTICATION_BACKENDS = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': config('DATABASE_PORT', default='5432'), # Add this line
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
